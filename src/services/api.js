aacd8d02db9fe25c06717588063eec0d8ecdbbfb
Suyog Dhepe <suyogdhepe18@gmail.com>
2026-02-27 04:38:19 +0530

    h

--- src/services/api.js
+++ src/services/api.js
@@ -9,14 +9,4 @@ const API = axios.create({
   },
 });
 
-API.interceptors.request.use((config) => {
-  const token = localStorage.getItem("access");
-
-  if (token) {
-    config.headers.Authorization = `Bearer ${token}`;
-  }
-
-  return config;
-});
-
 export default API;
